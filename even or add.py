9d7638074d45520f33f0c454b4a37e0978b4875e
maghe1898 <37238390+maghe1898@users.noreply.github.com>
2019-02-04 15:39:03 +0530

    Delete even or add.py

--- even or add.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Enter any number: "))
-flag = num%2
-if flag == 0:
-    print(num, "the num is an even number")
-elif flag == 1:
-    print(num, "the num is an odd number")
-else:
-    print("Invalid input")
