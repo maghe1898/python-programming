9cb0f49d84f7606483e26de8881aaff5ad275c6b
maghe1898 <37238390+maghe1898@users.noreply.github.com>
2019-02-04 15:58:58 +0530

    Create vowel or constant.py

---
+++ vowel or constant.py
@@ -0,0 +1,7 @@
+character=input("Enter a character: ")
+
+if(character=='A' or character=='a' or character=='E' or character =='e' or character=='I'
+ or character=='i' or character=='O' or character=='o' or character=='U' or character=='u'):
+    print(character, "is a Vowel")
+else:
+    print(character, "is a Consonant")
