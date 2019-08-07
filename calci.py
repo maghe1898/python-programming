4aa91abf59829d2e5ed0426f33ad9ab9a0181259
maghe1898 <37238390+maghe1898@users.noreply.github.com>
2019-08-07 13:10:01 +0530

    Create calci.py

---
+++ calci.py
@@ -0,0 +1,15 @@
+a=eval(input("enter value:"))
+b=eval(input("enter value:"))
+c=eval(input("enter operation:"))
+if c==1:
+    c=a+b
+    print(c)
+elif c==2:
+    c=a-b
+    print(c)
+elif c==3:
+    c=a*b
+    print(c)
+else:
+    c=a/b
+    print(c)
